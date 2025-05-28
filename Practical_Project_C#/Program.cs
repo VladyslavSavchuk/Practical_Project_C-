fbf2c886d6d42884940749f8c5f40715d9a590ef
Vlad <Vlad@VLADPC>
2025-05-28 03:07:43 +0300

    Add project files.

---
+++ Practical_Project_C#/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Practical_Project_C_
+{
+    /*
+     * I will do that project in morning, hope in deadline !!!
+     */
+
+    internal class Program
+    {
+        static void Main()
+        {
+            
+        }
+    }
+
+    abstract class Fruit
+    {
+        
+    }
+}
